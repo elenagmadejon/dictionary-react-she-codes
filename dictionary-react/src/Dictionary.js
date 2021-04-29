b9c4f9d3bf03c8759b02d87d1285e677ec089121
Pinguanda <pinguanda@WAWAs-MacBook-Pro.local>
2021-04-29 20:16:25 +0200

    Api integrated

--- dictionary-react/src/Dictionary.js
+++ dictionary-react/src/Dictionary.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import "./Dictionary.css";
+import Axios from "axios";
 
 export default function Dictionary(){
     let [keyword, setKeyWord] = useState("");
 
+    function handleResponse(response){
+        console.log(response);
+    }
+
     function search(event){
         event.preventDefault();
         alert(`Searching for ${keyword}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        Axios.get(apiUrl).then(handleResponse)
     }
+
         function handleKeywordChange(event){
             console.log(event.target.value);
             setKeyWord(event.target.value);
